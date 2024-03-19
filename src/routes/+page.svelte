d9bff4c91042b68b78edcba4753f0a345a461b2f
Marcel Wiessler <marcel@gaisterhand.de>
2024-03-19 21:56:41 +0100

    start

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import Peer from "peerjs";
+    import { onMount } from "svelte";
+
+    onMount(() => {
+        const room = new URLSearchParams(location.search).get("room");
+        console.log(room);
+        const peer = new Peer();
+        peer.on("open", (id) => {
+            console.log("My peer ID is: " + id);
+        });
+    });
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
